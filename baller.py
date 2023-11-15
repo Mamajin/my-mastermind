4d1be708f2fc6cd54618808a4f953214ac56076e
Phasit RUANGMAK <phasit2005@gmail.com>
2023-11-15 12:04:24 +0700

    Added the bases of the game by creating a method and the game is now in testing

--- baller.py
+++ baller.py
@@ -34,6 +34,11 @@ class Board:
 
     def check_guess(self, player_guess):
         """check user guess and give a hint"""
+        # [2, 5, 5, 1]
+        # What is your guess?: 1331
+        # Your guess is 1331
+        # o*
+        # debug the code above printing only the one with higher truth value
         hint = ""
         for i in range(len(self.answer)):
             if player_guess[i] == self.answer[i]:
@@ -44,8 +49,22 @@ class Board:
 
     def comp_ans_to_guess(self, player_guess):
         if self.answer == player_guess:
-            return False
+            return True
         else:
             pass
 
 
+print("Playing Mastermind with 6 colors and 4 positions")
+board = Board(6, 4)
+print(board.answer)
+game = True
+tries = 0
+while game:
+    guess_ls = board.get_guess()
+    hint = board.check_guess(guess_ls)
+    print(hint)
+    print()
+    tries += 1
+    if board.comp_ans_to_guess(guess_ls):
+        game = False
+print(f"You solve it after {tries} rounds")
